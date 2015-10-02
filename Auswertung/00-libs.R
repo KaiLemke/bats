d21c9d4fefb1e878ae483c701ba142abe77dc237
Kai Lemke <kai.lemke91@gmx.de>
2015-10-02 17:45:11 +0200

    data neu angefangen: Auswertung/read.R und gespeicherte Datensätze

--- Auswertung/00-libs.R
+++
@@ -1,3 +0,0 @@
-library(pander)
-library(plyr)
-
--- Auswertung/read.R
+++ Auswertung/read.R
@@ -1,31 +1,93 @@
-# Zuerst müssen wir die Gesamttabelle einlesen und die Datentypen gemäß werte.csv korrigieren.
-df <- read.csv("gesamttabelle.csv", header=T)
+# Einlesen der Gesamttabelle
+df <- read.csv("../gesamttabelle.csv", header=T)
 
-# korrigiere Datentypen
-df$gpsr  <- as.double(df$gpsr)
-df$gpsh  <- as.double(df$koa)
-df$koa  <- as.factor(df$koa)
-df$kod  <- as.factor(df$kod)
-df$wt  <- as.numeric(df$wt)
-df$vn  <- as.factor(df$vn)
-df$aves  <- as.factor(df$aves)
-df$mamm  <- as.factor(df$mamm)
-df$invert  <- as.factor(df$invert)
-df$flkot  <- as.factor(df$flkot)
-df$qtyp  <- as.factor(df$qtyp)
+# Korrigieren der Datenstruktur gemäß werte.csv
+
+str(df)
+
+# nicht verwertbare Variablen entfernen
+with(
+     df,
+     rm(kid),
+     rm(knr),
+     rm(gpsr),
+     rm(gpsh),
+     rm(sonstiges)
+     )
+str(df)
+
+# die unabhängigen Variablen
+x.schlag <- df$schlag
+x.ktyp <- df$ktyp
+x.koa <- df$koa
+x.kod <- df$kod
+x.wt <- df$wt
+x.wb <- factor(df$wb, levels=c("r", "w", "b", "s"))
+str(x.wb)
+head(x.wb); head(df$wb)
+x.kz <- factor(df$kz,levels=c("d", "sb", "lb", "i"))
+str(x.kz)
+head(x.kz); head(df$kz)
+x.vn <- factor(df$vn)
+x.aves <- factor(df$aves)
+x.mamm <- factor(df$mamm)
+x.invert <- factor(df$invert)
+x.flkot <- factor(df$flkot)
+x.qtyp <- factor(df$qtyp)
+x.flart <- df$flart
+
+# die abhängigen Variablen
+y.kz <- as.integer(x.kz)
+str(y.kz)
+head(x.kz); head(y.kz)
+y.vn <- df$vn
+head(y.vn); head(x.vn)
+y.flkot <- df$flkot
+y.flges <- df$flges
+y.flm <- df$flm
+y.flw <- df$flw
+y.flad <- df$flad
+y.fljuv <- df$fljuv
+
+# df mit neuen Variablen
+df <- data.frame(
+		 x.schlag,
+		 x.ktyp,
+		 x.koa,
+		 x.kod,
+		 x.wt,
+		 x.wb,
+		 x.kz,
+		 x.vn,
+		 x.aves,
+		 x.mamm,
+		 x.invert,
+		 x.flkot,
+		 x.qtyp,
+		 x.flart,
+		 y.kz,
+		 y.vn,
+		 y.flkot,
+		 y.flges,
+		 y.flm,
+		 y.flw,
+		 y.flad,
+		 y.fljuv
+		 )
+str(df)
 
 # Subsets für 2010 und 2011 bis 2015
 invisible(
 	  lapply(
 		 split(
 		       df, 
-		       df$koa
+		       df$x.koa
 		       ), 
 		 function(x) {
 			 assign(
 				paste0(
 				       "df", 
-				       x$koa[1]
+				       x$x.koa[1]
 				       ), 
 				x, 
 				pos=.GlobalEnv
@@ -45,3 +107,14 @@ l <- list(
 	  df15=df15
 	  )
 
+ls()
+
+# schreibe die Datensätze in Dateien
+save(df, file="df")
+save(df10, file="df10")
+save(df11, file="df11")
+save(df12, file="df12")
+save(df13, file="df13")
+save(df14, file="df14")
+save(df15, file="df15")
+save(l, file="l")
